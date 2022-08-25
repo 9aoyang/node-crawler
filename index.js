b946ba869bc66b5160fab74b38c15099d4da9c7f
silence1amb <9aoyang@gmail.com>
2022-08-25 16:52:26 +0800

    feat: 页面跳转

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
   const page = await browser.newPage();
-  await page.goto('https://www.baidu.com');
-  await page.screenshot({ path: 'baidu.png' });
+  await page.goto('https://zh.mindat.org/');
+  await page.type('#searchboxhp', 'Achyrophanite');
+
+  await Promise.all([
+    page.waitForNavigation(),
+    await page.keyboard.press('Enter'),
+  ]);
+
+  await page.screenshot({ path: 'page.png' });
 
   await browser.close();
 })();
