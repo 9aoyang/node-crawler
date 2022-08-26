cd42d84e4b0d401a8f3ff1a17713d2ba24fcd4a0
silence1amb <9aoyang@gmail.com>
2022-08-26 17:20:48 +0800

    feat: 获取 mindat 所需字段数据

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const crawler = require('./crawler');
 const json2csv = require('./json2csv');
 const cols = require('./cols');
 
+process.setMaxListeners(0);
+
 const fetchMineralListInfo = async (mineralList) => {
   const rows = [];
   const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 2,
+    concurrency: Cluster.CONCURRENCY_PAGE,
+    maxConcurrency: 40,
+    timeout: 24 * 60 * 60 * 1000,
     puppeteerOptions: {
-      headless: true,
+      // headless: false,
       args: [
+        '--disable-web-security',
+        '--disable-xss-auditor',
+        '--disable-webgl',
         '--disable-gpu', // GPU硬件加速
         '--disable-dev-shm-usage', // 创建临时文件共享内存
         '--disable-setuid-sandbox', // uid沙盒
@@ -40,16 +46,21 @@ const fetchMineralListInfo = async (mineralList) => {
     await page.setDefaultNavigationTimeout(0);
 
     await Promise.all([page.waitForNavigation(), await page.goto(url)]);
+    // await page.screenshot({ path: '1.png' });
 
     const details = await page.evaluate(crawler);
-    rows.push([name, details]);
+    rows.push([name, ...details]);
+
     console.log(`任务 ${index} 完成, 共 ${total} 个任务`);
   });
 
-  for (let i = 0, len = mineralList.length; i < 3; i++) {
+  const num = 5100;
+
+  for (let i = num, len = mineralList.length; i < len; i++) {
     const mineral = mineralList[i];
     cluster.queue({
       url: `https://zh.mindat.org/search.php?search=${mineral}`,
+      // url: `https://www.mindat.org/search.php?search=${mineral}`,
       index: i,
       total: len,
       name: mineral,
